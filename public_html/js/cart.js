032746869c458ed0a0199e71694bfaa787be7440
SGVcode <sgvero.box@gmail.com>
2023-12-09 03:05:35 -0300

    [FEAT] correcciones

--- public_html/js/cart.js
+++ public_html/js/cart.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
     // Agregar o quitar productos en el carrito Opc 1
     const addOne = document.querySelector("#addOne");
     const substractOne = document.querySelector("#substractOne");
@@ -8,59 +8,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
     substractOne.addEventListener('click', () => {
         const currentQuantityOne = Number(quantityOne.value);
-        if (currentQuantityOne > 1) {
+        if (currentQuantityOne > 0) {
             quantityOne.value = currentQuantityOne - 1;
         } else {
-            quantityOne.value = 1;
+            quantityOne.value = 0;
         }
     });
 });
-
-function updateQuantity(productId, action, cart) {
-    // Obtener la nueva cantidad después de la acción del usuario
-    const quantityInput = document.getElementById('quantityOne' + productId);
-    const newQuantity = parseInt(quantityInput.value);
-
-    // Enviar la solicitud al servidor para actualizar la cantidad
-    fetch(`/cart/${productId}`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ quantity: newQuantity }),
-    })
-        .then(response => {
-            if (response.ok) {
-                // Si la actualización en el servidor fue exitosa, recargar la página
-                location.reload();
-            } else {
-                console.error('Error al actualizar la cantidad del producto.');
-            }
-        })
-        .catch(error => {
-            console.error('Error en la solicitud fetch:', error);
-        });
-}
-
-function deleteItem(productId) {
-    console.log('Deleting product with ID:', productId);
-    fetch(`/shop/cart/${productId}/delete`, {
-        method: 'DELETE',
-        credentials: 'same-origin',  // Esto es importante para manejar cookies correctamente
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    })
-        .then(response => {
-            if (response.ok) {
-                // Actualizar la interfaz después de la eliminación exitosa
-                console.log('Producto eliminado exitosamente.');
-                location.reload();
-            } else {
-                console.error('Error al eliminar el producto del carrito.');
-            }
-        })
-        .catch(error => {
-            console.error('Error en la solicitud fetch:', error);
-        });
-}
